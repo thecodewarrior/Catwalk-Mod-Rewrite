4e54dff26ce8481d8aa9ffb0d7029572fcaf4a91
Pierce Corcoran <pierce@plasticcow.com>
2015-08-11 18:06:42 -0500

    Version change derp
    Change version from 0.1.1 to 0.1.2

--- src/main/java/com/thecodewarrior/catwalks/CatwalkMod.java
+++ src/main/java/com/thecodewarrior/catwalks/CatwalkMod.java
@@ -25,7 +25,7 @@ public class CatwalkMod
 {
     public static final String MODID = "catwalks";
     public static final String MODNAME = "Catwalks";
-    public static final String MODVER = "0.1.1";
+    public static final String MODVER = "0.1.2";
     
     @Instance(value = CatwalkMod.MODID)
     public static CatwalkMod instance;
